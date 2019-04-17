f825d0cb87ff54f01f8b8b2ca9444ba44f395782
aodhanman <aodhanman@users.noreply.github.com>
2019-04-18 00:00:16 +0100

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalApp
+{
+    public class Item : Models.BaseFodyObservable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
